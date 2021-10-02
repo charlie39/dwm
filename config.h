e2e63912651bb14a0c46abb9262ec6df660f8bab
charlie39 <iamkg050392@gmail.com>
2021-10-03 03:25:14 +0530

    increased font icon size

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Code:size=10", "JoyPixels:pixelsize=12:antialias=true:autohint=true"  };
+static const char *fonts[]          = { "Fira Code:size=12", "JoyPixels:pixelsize=16:antialias=true:autohint=true"  };
 static char dmenufont[]     	    = "monospace:size=10";
 /* static char normbgcolor[]           = "#00cd00"; */
 static char normbgcolor[]           = "#222222";
