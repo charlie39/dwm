47e79f61d82b5fea69e9b47c101b4c1850859443
charlie39 <iamkg050392@gmail.com>
2021-10-03 12:41:47 +0530

    final set up of status bar nerd fonts

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Code:size=12", "FontAwesome:bold:size=26:antialias=true:autohint=true"  };
+static const char *fonts[]          = { "Fira Code:bold:size=10","Symbols Nerd Font:bold:size=14:autohint=true", "JoyPixels:pixelsize=12"  };
 static char dmenufont[]     	    = "monospace:size=10";
 /* static char normbgcolor[]           = "#00cd00"; */
 static char normbgcolor[]           = "#222222";
@@ -27,8 +27,9 @@ static char *colors[][3] = {
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
-/* tagging */
-static const char *tags[] = { "", "", "", "", "5", "", "", "", "" };
+/* tagging*/
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+// static const char *tags[] = { "", "", "", "", "5", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
